f81697a408bc083ce00aa25ec873ae34405a21e6
Kavya Gupta <rkgqwe@gmail.com>
2023-08-12 15:04:44 +0530

    Update Ecommerce.jsx

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -34,7 +34,7 @@ const Ecommerce = () => {
 
                 <p className='mt-3'>
                   <span className='text-lg font-semibold'>{item.amount}</span>
-                  <span className={`text-sm text-red-600 ml-2`}>{item.percentage}</span>
+                  <span className={`text-sm text-${item.pcColor} ml-2`}>{item.percentage}</span>
                 </p>
                 <p className='text-sm text-gray-400 mt-1'>
                   {item.title}
