0c0065a5bd7da9719b747a261daa627d7e69ceb4
Kavya Gupta <rkgqwe@gmail.com>
2023-08-12 15:01:01 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,6 +7,7 @@ import { useStateContext } from "../contexts/ContextProvider.jsx";
 
 const Sidebar = () => {
   const { activeMenu, setActiveMenu, screenSize, currentColor } = useStateContext();
+
   const activeLink =
     "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
   const normalLink =
